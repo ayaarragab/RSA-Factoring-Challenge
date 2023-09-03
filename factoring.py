ca388507e19b59bad01475aca5c11c58ff4f400b
ayaarragab <ayarsk4343@gmail.com>
2023-09-03 15:16:40 +0300

    RSA2

--- factoring.py
+++ factoring.py
@@ -1,7 +1,9 @@
 #!/bin/usr/python3
 import math
 def factor(n):
-    for i in range(2, int(math.sqrt(n))):
+    if n < 2:
+        print(f"{n}=1*{n}")
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             print(f"{n}={n // i}*{i}")
             break
